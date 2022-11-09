df7653e2f1d70fe7399c0ea6c36df452efc117a9
ucsg-teleclases <117224947+ucsg-teleclases@users.noreply.github.com>
2022-11-09 08:24:24 -0500

    Adición módulos

--- catolica/sistema-cardiovascular/rompecabezas/puzzle.js
+++ catolica/sistema-cardiovascular/rompecabezas/puzzle.js
@@ -4,38 +4,31 @@ var columns = 5;
 var currTile;
 var otherTile;
 
-var turns = 0;
-
 window.onload = function() {
-    //initialize the 5x5 board
     for (let r = 0; r < rows; r++) {
         for (let c = 0; c < columns; c++) {
-            //<img>
             let tile = document.createElement("img");
             tile.src = "./images/blank.jpg";
 
-            //DRAG FUNCTIONALITY
-            tile.addEventListener("dragstart", dragStart); //click on image to drag
-            tile.addEventListener("dragover", dragOver);   //drag an image
-            tile.addEventListener("dragenter", dragEnter); //dragging an image into another one
-            tile.addEventListener("dragleave", dragLeave); //dragging an image away from another one
-            tile.addEventListener("drop", dragDrop);       //drop an image onto another one
-            tile.addEventListener("dragend", dragEnd);      //after you completed dragDrop
+             tile.addEventListener("dragstart", dragStart); 
+            tile.addEventListener("dragover", dragOver);   
+            tile.addEventListener("dragenter", dragEnter); 
+            tile.addEventListener("dragleave", dragLeave);
+            tile.addEventListener("drop", dragDrop);       
+            tile.addEventListener("dragend", dragEnd);      
 
             document.getElementById("board").append(tile);
         }
     }
 
-    //pieces
     let pieces = [];
     for (let i=1; i <= rows*columns; i++) {
-        pieces.push(i.toString()); //put "1" to "25" into the array (puzzle images names)
+        pieces.push(i.toString()); 
     }
     pieces.reverse();
     for (let i =0; i < pieces.length; i++) {
         let j = Math.floor(Math.random() * pieces.length);
 
-        //swap
         let tmp = pieces[i];
         pieces[i] = pieces[j];
         pieces[j] = tmp;
@@ -45,21 +38,19 @@ window.onload = function() {
         let tile = document.createElement("img");
         tile.src = "./images/" + pieces[i] + ".jpg";
 
-        //DRAG FUNCTIONALITY
-        tile.addEventListener("dragstart", dragStart); //click on image to drag
-        tile.addEventListener("dragover", dragOver);   //drag an image
-        tile.addEventListener("dragenter", dragEnter); //dragging an image into another one
-        tile.addEventListener("dragleave", dragLeave); //dragging an image away from another one
-        tile.addEventListener("drop", dragDrop);       //drop an image onto another one
-        tile.addEventListener("dragend", dragEnd);      //after you completed dragDrop
+        tile.addEventListener("dragstart", dragStart); 
+        tile.addEventListener("dragover", dragOver);   
+        tile.addEventListener("dragenter", dragEnter); 
+        tile.addEventListener("dragleave", dragLeave); 
+        tile.addEventListener("drop", dragDrop);      
+        tile.addEventListener("dragend", dragEnd);     
 
         document.getElementById("pieces").append(tile);
     }
 }
 
-//DRAG TILES
 function dragStart() {
-    currTile = this; //this refers to image that was clicked on for dragging
+    currTile = this; 
 }
 
 function dragOver(e) {
@@ -75,7 +66,7 @@ function dragLeave() {
 }
 
 function dragDrop() {
-    otherTile = this; //this refers to image that is being dropped on
+    otherTile = this; 
 }
 
 function dragEnd() {
@@ -85,8 +76,5 @@ function dragEnd() {
     let currImg = currTile.src;
     let otherImg = otherTile.src;
     currTile.src = otherImg;
-    otherTile.src = currImg;
-
-    turns += 1;
-    document.getElementById("turns").innerText = turns;
+    otherTile.src = currImg; 
 }
--- catolica/sistema-locomotor/cuestionario/js/script.js
+++ catolica/sistema-locomotor/cuestionario/js/script.js
@@ -1,4 +1,3 @@
-//selecting all required elements
 const start_btn = document.querySelector(".start_btn button");
 const info_box = document.querySelector(".info_box");
 const exit_btn = info_box.querySelector(".buttons .quit");
@@ -10,17 +9,14 @@ const time_line = document.querySelector("header .time_line");
 const timeText = document.querySelector(".timer .time_left_txt");
 const timeCount = document.querySelector(".timer .timer_sec");
 
-// if startQuiz button clicked
 start_btn.onclick = ()=>{
     info_box.classList.add("activeInfo"); //show info box
 }
 
-// if exitQuiz button clicked
 exit_btn.onclick = ()=>{
     info_box.classList.remove("activeInfo"); //hide info box
 }
 
-// if continueQuiz button clicked
 continue_btn.onclick = ()=>{
     info_box.classList.remove("activeInfo"); //hide info box
     quiz_box.classList.add("activeQuiz"); //show quiz box
@@ -96,8 +92,8 @@ function showQuetions(index){
     let que_tag = '<span>'+ questions[index].numb + ". " + questions[index].question +'</span>';
     let option_tag = '<div class="option"><span>'+ questions[index].options[0] +'</span></div>'
     + '<div class="option"><span>'+ questions[index].options[1] +'</span></div>'
-    + '<div class="option"><span>'+ questions[index].options[2] +'</span></div>';
-    // + '<div class="option"><span>'+ questions[index].options[3] +'</span></div>';
+    + '<div class="option"><span>'+ questions[index].options[2] +'</span></div>'
+    + '<div class="option"><span>'+ questions[index].options[3] +'</span></div>';
     que_text.innerHTML = que_tag; //adding new span tag inside que_tag
     option_list.innerHTML = option_tag; //adding new div tag inside option_tag
     
